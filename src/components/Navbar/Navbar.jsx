e2059a112a8ecb898c6e02c7c15b1737be66d0a4
babywipez <cassada.chris@gmail.com>
2023-09-05 19:34:34 -0400

    add css vars and basic navbar components

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,10 +2,19 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <h1 className="navbar-title-color">Navbar</h1>
+        <div>
+            <p className="headline">L.M.</p>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Location</li>
+                    <li>Portfolio</li>
+                    <li>Booking</li>
+                    <li>Aftercare</li>
+                </ul>
+            </nav>
+        </div>
     )
 }
 
 export default Navbar
-
-
