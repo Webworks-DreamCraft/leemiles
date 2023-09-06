5921bf8ee2a27f09bc68cbf76aac038428a37088
Wyatt Rajer <wyattrajer@gmail.com>
2023-09-05 21:40:47 -0400

    Added Slider / added some images to assets folder for Slider test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import Navbar from './components/Navbar/Navbar'
+<<<<<<< HEAD
+=======
+import Header from './components/Header/Header'
+import Slider from './components/Slider/Slider'
+>>>>>>> b459b3a (Added Slider / added some images to assets folder for Slider test)
 import Contact from "./components/Contact/Contact"
 
 function App() {
@@ -6,6 +11,7 @@ function App() {
   return (
     <div className="container mx-auto">
       <Navbar />
+<<<<<<< HEAD
       <h1 className="title">
         LEE MILES
       </h1>
@@ -25,6 +31,17 @@ function App() {
         LEE MILES
       </h1>
       <h1 className="title transparent">LEE MILES</h1>
+=======
+      <Header />
+      <Slider />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!!!!!!======this works
+      </h1>g
+ 
+      <img src={reactLogo} className="logo react" alt="React logo" />
+      <img src={viteLogo} className="logo" alt="Vite logo" />
+ 
+>>>>>>> b459b3a (Added Slider / added some images to assets folder for Slider test)
       <Contact />
     </div>
   )
---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,36 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import ss1 from '../../assets/ss1.png'
+import ss2 from '../../assets/ss2.png'
+import ss3 from '../../assets/ss3.png'
+import ss4 from '../../assets/ss4.png'
+import ss5 from '../../assets/ss5.png'
+import wojack from '../../assets/wojack.gif'
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+const Slider = () => {
+  return (
+    <Swiper
+      modules={[Navigation]}
+      spaceBetween={50}
+      slidesPerView={3}
+      loop={true}
+      navigation
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide><img src={ss1} /></SwiperSlide>
+      <SwiperSlide><img src={ss2} /></SwiperSlide>
+      <SwiperSlide><img src={ss3} /></SwiperSlide>
+      <SwiperSlide><img src={ss4} /></SwiperSlide>
+      <SwiperSlide><img src={ss5} /></SwiperSlide>
+      <SwiperSlide><img src={wojack} /></SwiperSlide>
+    </Swiper>
+  );
+}
+
+export default Slider
