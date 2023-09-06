7846de180f4cfc303f0ea78558067f390fb892c2
babywipez <cassada.chris@gmail.com>
2023-09-06 00:01:17 -0400

    add nav functionality and rmv Header directory

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,19 +2,19 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <div>
-            <p className="headline">L.M.</p>
+        <div className='navbar-container' id="top">
+            <a href="#"><p className="headline">L.M.</p></a>
             <nav>
-                <ul>
-                    <li>Home</li>
-                    <li>Location</li>
-                    <li>Portfolio</li>
-                    <li>Booking</li>
-                    <li>Aftercare</li>
+                <ul className="navbar">
+                    <a href="#"><li>Home</li></a>
+                    <a href="#2"><li>Location</li></a>
+                    <a href="#3"><li>Portfolio</li></a>
+                    <a href="#4"><li>Booking</li></a >
+                    <a href="#5"><li>Aftercare</li></a >
                 </ul>
             </nav>
         </div>
-    )
+    )   
 }
 
 export default Navbar
