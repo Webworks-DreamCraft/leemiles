a3edc795ece647952ef9a07bcd36c2c95afc016c
Wyatt Rajer <wyattrajer@gmail.com>
2023-09-06 22:15:57 -0400

    Instagram fetch working

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,19 +1,32 @@
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import ss1 from '../../assets/ss1.png'
-import ss2 from '../../assets/ss2.png'
-import ss3 from '../../assets/ss3.png'
-import ss4 from '../../assets/ss4.png'
-import ss5 from '../../assets/ss5.png'
-import wojack from '../../assets/wojack.gif'
+import { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import ss1 from "../../assets/ss1.png";
+import ss2 from "../../assets/ss2.png";
+import ss3 from "../../assets/ss3.png";
+import ss4 from "../../assets/ss4.png";
+import ss5 from "../../assets/ss5.png";
+import wojack from "../../assets/wojack.gif";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import './Slider.css';
+import "swiper/css";
+import "swiper/css/navigation";
+import "./Slider.css";
 
 const Slider = () => {
+  const [instagramPics, setInstagramPics] = useState([]);
+
+  useEffect(() => {
+    const fetchInstagram = async () => {
+      const response = await fetch(
+        `https://graph.instagram.com/me/media?fields=media_url&access_token=${import.meta.env.VITE_LONG_TOKEN}`
+      );
+      const insta = await response.json();
+      console.log(insta);
+    };
+    fetchInstagram();
+  }, []);
+
   return (
     <Swiper
       modules={[Navigation]}
@@ -21,17 +34,29 @@ const Slider = () => {
       slidesPerView={3}
       loop={true}
       navigation
-      onSlideChange={() => console.log('slide change')}
+      onSlideChange={() => console.log("slide change")}
       onSwiper={(swiper) => console.log(swiper)}
     >
-      <SwiperSlide><img src={ss1} /></SwiperSlide>
-      <SwiperSlide><img src={ss2} /></SwiperSlide>
-      <SwiperSlide><img src={ss3} /></SwiperSlide>
-      <SwiperSlide><img src={ss4} /></SwiperSlide>
-      <SwiperSlide><img src={ss5} /></SwiperSlide>
-      <SwiperSlide><img src={wojack} /></SwiperSlide>
+      <SwiperSlide>
+        <img src={ss1} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={ss2} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={ss3} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={ss4} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={ss5} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={wojack} />
+      </SwiperSlide>
     </Swiper>
   );
-}
+};
 
-export default Slider
+export default Slider;
