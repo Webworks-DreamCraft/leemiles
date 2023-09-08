add030be9704a9807509b31edeb47447ceacab82
Wyatt Rajer <wyattrajer@gmail.com>
2023-09-07 22:18:36 -0400

    fuck off

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,43 @@
 import Navbar from './components/Navbar/Navbar'
+import Slider from './components/Slider/Slider'
+
+function App() {
+
+  return (
+    <div className="container mx-auto">
+      <Navbar />
+      <h1 className="title">
+        LEE MILES
+      </h1>
+      <h1 className="title" id="1">
+        LEE MILES
+      </h1>
+      <h1 className="title" id="2">
+        LEE MILES
+      </h1>
+      <h1 className="title" id="3">
+        LEE MILES
+      </h1>
+      <h1 className="title" id="4">
+        LEE MILES
+      </h1>
+      <h1 className="title" id="5">
+        LEE MILES
+      </h1>
+      <h1 className="title">LEE MILES</h1>
+      <Slider />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!!!!!!======this works
+      </h1>g
+ 
+      
+ 
+    </div>
+  )
+}
+
+export default App
+import Navbar from './components/Navbar/Navbar'
 <<<<<<< HEAD
 =======
 import Header from './components/Header/Header'
