aaece15ea780691abbdfd4e06315842c3d370c81
babywipez <cassada.chris@gmail.com>
2023-09-20 13:05:51 -0400

    add aftercare component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import Navbar from './components/Navbar/Navbar'
-import Slider from './components/Slider/Slider'
-import Contact from "./components/Contact/Contact"
-import Location from "./components/Location/Location"
+import Navbar from "./components/Navbar/Navbar";
+import Slider from "./components/Slider/Slider";
+import Contact from "./components/Contact/Contact";
+import Location from "./components/Location/Location";
+import Aftercare from "./components/Aftercare/Aftercare";
 
 function App() {
-
   return (
     <div className="container mx-auto">
       <Navbar />
       <Slider />
       <Location />
+      <Aftercare />
       <Contact />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
