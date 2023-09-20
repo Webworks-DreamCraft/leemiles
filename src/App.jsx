ebde3953dbd5ddde4e9eb5a18aba0ec23118e70c
lukepate <luke.pate@vice.com>
2023-09-19 22:18:02 -0400

    feat(header) add header

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Navbar from './components/Navbar/Navbar'
 import Slider from './components/Slider/Slider'
 import Contact from "./components/Contact/Contact"
 import Location from "./components/Location/Location"
+import Header from "./components/Header/Header"
 
 function App() {
 
   return (
     <div className="container mx-auto">
       <Navbar />
+      <Header />
       <Slider />
       <Location />
       <Contact />
