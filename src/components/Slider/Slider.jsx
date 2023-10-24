d5c255283a45352e5f94e1804da98524b8d72583
lukepate <luke.pate@vice.com>
2023-10-24 18:59:09 -0400

    update ig images

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -21,7 +21,7 @@ const Slider = () => {
       try {
         const response = await fetch(
           `https://graph.instagram.com/me/media?fields=media_url,media_type&access_token=${
-            import.meta.env.VITE_LONG_TOKEN
+            process.env.VITE_LONG_TOKEN
           }`
         );
         const insta = await response.json();
