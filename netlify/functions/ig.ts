93ef0a0a5ceeed7b01ef9138c318cca6376e6912
lukepate <luke.pate@vice.com>
2024-01-30 22:44:32 -0500

    test scheduler

--- netlify/functions/ig.ts
+++ netlify/functions/ig.ts
@@ -7,8 +7,9 @@ const clientPromise = mongoClient.connect();
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   try {
     const database = (await clientPromise).db('webworks');
-    const collection = database.collection('tokens');
-    const results = await collection.find({}).limit(10).toArray();
+    // const collection = database.collection('tokens');
+    // const results = await collection.find({}).limit(10).toArray();
+    const results = 'test'
     const value = process.env.MY_IMPORTANT_VARIABLE;
     return {
       statusCode: 200,
