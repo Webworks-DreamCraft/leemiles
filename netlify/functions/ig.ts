7099831b0d9d29afd07fd0a666493a90dabcff6f
lukepate <luke.pate@vice.com>
2024-01-30 22:34:01 -0500

    test scheduler

--- netlify/functions/ig.ts
+++ netlify/functions/ig.ts
@@ -1,13 +1,23 @@
 import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+import { MongoClient } from "mongodb";
+
+const mongoClient = new MongoClient(process.env.MONGODB_URI);
+const clientPromise = mongoClient.connect();
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-  
-  const value = process.env.VITE_MONGO;
+  try {
+    const database = (await clientPromise).db('webworks');
+    const collection = database.collection('tokens');
+    const results = await collection.find({}).limit(10).toArray();
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: `Value of MY_IMPORTANT_VARIABLE is ${value}.`}),
-  };  
+    const value = process.env.MY_IMPORTANT_VARIABLE;
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Value of MY_IMPORTANT_VARIABLE is ${value}. ${results}`}),
+    };  
+  } catch (error) {
+      return { statusCode: 500, body: error.toString() }
+  }
 };
 
 export { handler };
