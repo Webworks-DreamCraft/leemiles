97f695c77e580fc544b23243271ecbe67a729b20
lukepate <luke.pate@vice.com>
2024-01-31 22:42:03 -0500

    test function

--- netlify/functions/ig.ts
+++ netlify/functions/ig.ts
@@ -1,51 +1,44 @@
 import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 import { MongoClient } from "mongodb";
+import fetch from 'node-fetch';
 
 const MONGODB_URI = process.env.MONGODB_DATABASE;
 const DB_NAME = 'webworks';
 const value = process.env.MY_IMPORTANT_VARIABLE;
 let cachedDb = null;
 
-const connectToDatabase = async (uri) => {
-  // we can cache the access to our database to speed things up a bit
-  // (this is the only thing that is safe to cache here)
-  if (cachedDb) return cachedDb;
-
-  const client = await MongoClient.connect(uri, {
-    useUnifiedTopology: true,
+async function getData() {
+  const client = new MongoClient(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   });
 
-  cachedDb = client.db(DB_NAME);
-
-  return cachedDb;
-};
-
-const queryDatabase = async (db) => {
-  const pokemon = await db.collection("tokens").find({}).toArray();
-
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(pokemon),
-  };
-};
-
-const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   try {
-  context.callbackWaitsForEmptyEventLoop = false;
+    await client.connect();
+    const test = await client
+      .db('webworks')
+      .collection('tokens')
+      .find();
+    return test;
+  } catch (err) {
+    console.log(err); // output to netlify function log
+  } finally {
+    await client.close();
+  }
+}
 
-  const db = await connectToDatabase(MONGODB_URI);
-  const results = queryDatabase(db);
-
+exports.handler = async function(event, context) {
+  try {
+    const data = await getData();
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Value of MY_IMPORTANT_VARIABLE is ${value}. ${results}`}),
-    };  
-  } catch (error) {
-      return { statusCode: 500, body: error.toString() }
+      body: JSON.stringify({ id: data._id })
+    };
+  } catch (err) {
+    console.log(err); // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message }) 
+    };
   }
 };
-
-export { handler };
