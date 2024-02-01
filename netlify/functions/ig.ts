87abdb2e6acbeaa687319f8cd5a1ae63c6fc3616
lukepate <luke.pate@vice.com>
2024-01-31 23:23:29 -0500

    test function

--- netlify/functions/ig.ts
+++ netlify/functions/ig.ts
@@ -1,33 +1,61 @@
-import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 import { MongoClient, ServerApiVersion } from "mongodb";
-const MONGO_HOST = process.env.MONGODB_DATABASE;
-const DB_NAME = 'webworks';
 const MONGODB_COLLECTION = 'tokens';
 const value = process.env.MY_IMPORTANT_VARIABLE;
+const MONGODB_URI = process.env.MONGODB_DATABASE;
 
-export const mongoClient = new MongoClient(MONGO_HOST, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
+let cachedDb = null;
 
-const clientPromise = mongoClient.connect();
+const connectToDatabase = async (uri) => {
+  if (cachedDb) return cachedDb;
 
-const connectDB = async () => {
-  try {
-    const database = (await clientPromise).db(DB_NAME);
-    const collection = database.collection(MONGODB_COLLECTION);
-    console.log("[db] Conectada con éxito", collection);
+  const client = await MongoClient.connect(uri, {
+    useUnifiedTopology: true,
+  });
 
-    const results = await collection.find({}) || 'found nothing';
-    return {
-      statusCode: 200,
-      body: JSON.stringify(results),
-    };
-  } catch (err) {
-    console.error("[db] Error", MONGO_HOST, err);
-    return { statusCode: 500, body: err.toString() };
+  cachedDb = client.db("webworks");
+
+  return cachedDb;
+};
+
+const queryDatabase = async (db) => {
+  const pokemon = await db.collection("tokens").find({}).toArray();
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(pokemon),
+  };
+};
+
+const pushToDatabase = async (db, data) => {
+  const pokemonData = {
+    name: data.name,
+    number: data.number,
+  };
+
+  if (pokemonData.name && pokemonData.number) {
+    await db.collection("pokemon").insertMany([data]);
+    return { statusCode: 201 };
+  } else {
+    return { statusCode: 422 };
   }
 };
 
-export default connectDB;
+module.exports.handler = async (event, context) => {
+  // otherwise the connection will never complete, since
+  // we keep the DB connection alive
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const db = await connectToDatabase(MONGODB_URI);
+
+  switch (event.httpMethod) {
+    case "GET":
+      return queryDatabase(db);
+    case "POST":
+      return pushToDatabase(db, JSON.parse(event.body));
+    default:
+      return { statusCode: 400 };
+  }
+};
